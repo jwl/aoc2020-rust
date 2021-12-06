3a7f47ecccaf98b47cde6a24eefb77eb9a382ad2
jwl <james.warren.leung@pm.me>
2021-12-06 14:43:52 -0800

    reorganized to start from day 17 onward

---
+++ README.md
@@ -0,0 +1,12 @@
+# aoc2020rust
+
+My Advent of Code 2020 solutions, in rust. Only contains solutions for day 17 onward, since 16 and below are found in https://github.com/jwl/adventofcode2020
+
+Each problem contains an `input.txt` containing the problem input.
+
+To run a solution:
+```
+# Switch to day 1a and run
+cd day01a
+cargo run --release
+```
---
+++ aocutils/src/lib.rs
@@ -0,0 +1,52 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Error, ErrorKind};
+use std::path::Path;
+
+pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+pub fn load_input_as_ints(filename: String) -> Result<Vec<i64>, Error> {
+    let mut vec = Vec::new();
+    if let Ok(lines) = read_lines(filename) {
+        for line in lines {
+            vec.push(
+                line?
+                    .trim()
+                    .parse()
+                    .map_err(|e| Error::new(ErrorKind::InvalidData, e))?,
+            );
+        }
+    }
+
+    Ok(vec)
+}
+
+pub fn load_input_as_strings(filename: String) -> Vec<String> {
+    let file = File::open(filename).unwrap();
+    BufReader::new(file)
+        .lines()
+        .collect::<Result<_, _>>()
+        .unwrap()
+}
+
+// Read in file with an int in each line and and output a Vector
+pub fn read_input_as_ints(filename: String) -> Vec<i64> {
+    let file = File::open(filename).unwrap();
+    BufReader::new(file)
+        .lines()
+        .map(|line| line.unwrap().parse::<i64>().unwrap())
+        .collect()
+}
+
+pub fn read_input_as_binaryints(filename: String) -> Vec<i64> {
+    let file = File::open(filename).unwrap();
+    BufReader::new(file)
+        .lines()
+        .map(|line| i64::from_str_radix(line.unwrap().as_str(), 2).unwrap())
+        .collect()
+}
